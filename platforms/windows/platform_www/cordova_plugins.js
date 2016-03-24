f8dc1c9ea0413cb883d39ffd3c4acc2837b3dbd9
mariuszmatusiak <azamennace@gmail.com>
2016-03-24 20:51:36 +0100

    init

---
+++ platforms/windows/platform_www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/src/windows/NotificationProxy.js",
+        "id": "cordova-plugin-dialogs.NotificationProxy",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
